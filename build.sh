9977434b82037ec48420fefaadc69a44df772257
Cody Allen <ceedubs@gmail.com>
2022-01-18 09:47:28 -0800

    Automate codebase update via GitHub Actions
    paired with @hojberg

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ rm -rf .unison
 rm -rf build
 
 mkdir build
-TMPDIR=build unison transcript init-share.md --save-codebase
+TMPDIR=build ucm transcript init-share.md --save-codebase
 output=$(ls build | head -n 1)
 cp -R build/${output}/.unison .
